372c2b221d8bd828d10ec7d2a1852ce3e28454dc
ClushAKvK <90634189+ClushAKvK@users.noreply.github.com>
2022-10-15 00:16:08 +0300

    Delete NumericalMethod/src/com/company/Saska/task4 directory

--- NumericalMethod/src/com/company/Saska/task4/Main/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company.Saska.task4.Main;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Program program = new Program();
-        program.start();
-    }
-}
